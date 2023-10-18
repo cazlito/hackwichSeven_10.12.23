c8729c2b6772a9ef8938100b37242dc592e59c85
cazlito <142947671+cazlito@users.noreply.github.com>
2023-10-17 20:54:49 -1000

    completed problem sets

--- hackwichSeven_10.12.23/ViewController.swift
+++ hackwichSeven_10.12.23/ViewController.swift
@@ -8,7 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet var displayLabel: UILabel!
+    
+    @IBAction func setLabelButtonPressed(_ sender: Any) {
+        if let userText = textField.text {
+            displayLabel.text = userText
+          }
+      }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
